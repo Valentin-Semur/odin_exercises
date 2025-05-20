1092c4ec624fa317b807ab2ae1da324e762f8bae
Valentin Semur <valentin@Valentins-Mac-mini.local>
2025-05-20 08:40:18 +0200

    feat: solve fibonacci iteration and recursive

---
+++ full-stack-javascript/javascript/project-recursion/script.js
@@ -0,0 +1,25 @@
+function fibs(num) {
+    var fibonacciSequence = [0, 1];
+    if (num == 1) {
+        return [0];
+    } else if (num == 2) {
+        return fibonacciSequence;
+    }
+    for (var step = 2; step < num; step++) {
+        var sequenceLength = fibonacciSequence.length;
+        fibonacciSequence.push(fibonacciSequence[sequenceLength - 1] + fibonacciSequence[sequenceLength - 2]);
+    }
+    return fibonacciSequence
+}
+
+function fibsRec(num, fibonacciSequence = [0, 1]) {
+    if (num == 1) {
+        return [0];
+    } else if (num == 2) {
+        return fibonacciSequence;
+    }
+    var sequenceLength = fibonacciSequence.length;
+    fibonacciSequence.push(fibonacciSequence[sequenceLength - 1] + fibonacciSequence[sequenceLength - 2]);
+    return fibsRec(num - 1, fibonacciSequence)
+}
+
