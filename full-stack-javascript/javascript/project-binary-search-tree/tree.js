7589a14bfeea4f0d3b3c5d02180cc27e0ea64e40
Valentin Semur <valentin@Valentins-Mac-mini.local>
2025-05-21 14:34:15 +0200

    feat: add insert and delete methods

--- full-stack-javascript/javascript/project-binary-search-tree/tree.js
+++ full-stack-javascript/javascript/project-binary-search-tree/tree.js
@@ -27,10 +27,71 @@ const Tree = (array) => {
         return a - b;
     }
 
+    function insertRecursive(value, currentNode) {
+        if (currentNode === null) {
+            return Node(value);
+        }
+
+        if (currentNode.data > value) {
+            currentNode.left = insertRecursive(value, currentNode.left);
+        } else if (currentNode.data < value) {
+            currentNode.right = insertRecursive(value, currentNode.right);
+        }
+        // Implicitely doing nothing when the value already exists in the tree
+
+        return currentNode;
+    }
+
+    const insert = (value) => {
+        // Check if value does not already exists ?
+        
+        root = insertRecursive(value, root);
+        return true;
+    }
+
+    function findSuccessor(currentNode) {
+        currentNode = currentNode.right;
+        while (currentNode !== null && currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode;
+    }
+
+    function deleteItemRecursive(value, currentNode) {
+        if (currentNode === null) {
+            console.log("Value does not exist in tree");
+            return currentNode;
+        }
+
+        if (currentNode.data > value) {
+            currentNode.left = deleteItemRecursive(value, currentNode.left);
+        } else if (currentNode.data < value) {
+            currentNode.right = deleteItemRecursive(value, currentNode.right);
+        } else {
+            // When node has not children or only right child
+            if (currentNode.left === null) {
+                return currentNode.right;
+            }
+
+            // When node has only left child
+            if (currentNode.right === null) {
+                return currentNode.left;
+            }
+
+            // When node has two children
+            let successor = findSuccessor(currentNode);
+            currentNode.data = successor.data;
+            currentNode.right = deleteItemRecursive(successor.data, currentNode.right);
+        }
+
+        return currentNode;
+    }
 
 
-
-
+    const deleteItem = (value) => {
+        root = deleteItemRecursive(value, root);
+        return true
+    }
 
     const prettyPrint = (node, prefix = "", isLeft = true) => {
         if (node === null) {
@@ -45,11 +106,13 @@ const Tree = (array) => {
         }
       };
 
-    return { root, prettyPrint }
+    return { root, prettyPrint, insert, deleteItem}
 }
 
 
 
 const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let testTree = Tree(testArray);
+testTree.prettyPrint(testTree.root)
+testTree.deleteItem(8)
 testTree.prettyPrint(testTree.root)
