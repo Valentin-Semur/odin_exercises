83fb77a7875c54bdb6cbb9a548d9215d92741482
Valentin Semur <valentin.semur@gmail.com>
2025-05-29 14:47:10 +0200

    feat: add calculator and caesarCypher methods and their tests

--- full-stack-javascript/javascript/project_testing-practice/src/main.js
+++ full-stack-javascript/javascript/project_testing-practice/src/main.js
@@ -6,3 +6,84 @@ export const reverseString = (string) => {
     return string.split('').reverse().join('');
 }
 
+export const Calculator = () => {
+    
+    const add = (a, b) => {
+        return a + b;
+    };
+
+    const subtract = (a, b) => {
+        return a - b;
+    };
+
+    const divide = (a, b) => {
+        if (b === 0) {
+            throw new Error('division by zero');
+        };
+        return a / b;
+    };
+
+    const multiply = (a, b) => {
+        return a * b;
+    };
+
+    return { add, subtract, divide, multiply }
+}
+
+export const ceasarCipher = () => {
+
+    const abc = 'abcdefghijklmnopqrstuvwxyz';
+    const upperAbc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    function isUpperCase(letter) {
+        if (letter === letter.toUpperCase()) {
+            return true;
+        }
+        return false;
+    }
+
+    function encodeLetter(letter, shiftFactor) {
+        // Check if the first argument is a letter, otherwise return it unchanged
+        if (!letter.match(/[a-z]/i)) {
+            return letter;
+        };
+
+        let letterIsUpperCase = false;
+
+        if (isUpperCase(letter)) {
+            letterIsUpperCase = true;
+            letter = letter.toLowerCase();
+        }
+
+        let currentPosition = abc.indexOf(letter);
+        let newPosition = currentPosition + shiftFactor;
+
+        // Shift back to the start of the alphabet
+        if (newPosition > 25) {
+            newPosition = newPosition - 26;
+        }
+
+        if (letterIsUpperCase) {
+            return upperAbc[newPosition];
+        } else if (!letterIsUpperCase) {
+            return abc[newPosition];
+        };
+    }
+
+    const encode = (string, shiftFactor) => {
+        if (typeof(string) !== 'string') {
+            throw new Error('first argument must be a string');
+        } else if (typeof(shiftFactor) !== 'number') {
+            throw new Error('second argument must be a number');
+        };
+
+        const encodedString = string
+            .split('')
+            .map(letter => encodeLetter(letter, shiftFactor))
+            .join('');
+
+        return encodedString
+    }
+
+    return { encode }
+}
--- full-stack-javascript/javascript/project_testing-practice/src/test/main.test.js
+++ full-stack-javascript/javascript/project_testing-practice/src/test/main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "../main.js";
+import { capitalize, reverseString, Calculator, ceasarCipher } from "../main.js";
 
 describe('capitalize', () => {
     it('is defined', () => {
@@ -34,4 +34,106 @@ describe('reverseString', () => {
     it('returns the correct string 2', () => {
         expect(reverseString('glop')).toBe('polg')
     });
+})
+
+describe('calculator', () => {
+
+    let myCalculator;
+
+    beforeEach(() => {
+        myCalculator = Calculator();
+    });
+
+    it('is defined', () => {
+        expect(Calculator).toBeDefined();
+    });
+
+    it('creates an object', () => {
+        expect(typeof(myCalculator)).toBe('object')
+    });
+
+    it('has an add methods', () => {
+        expect(myCalculator.add).toBeDefined();
+    })
+
+    it('adds two numbers correctly', () => {
+        expect(myCalculator.add(1,1)).toEqual(2);
+    });
+
+    it('has a subtract method', () => {
+        expect(myCalculator.subtract).toBeDefined();
+    });
+
+    it('subtract two numbers correctly', () => {
+        expect(myCalculator.subtract(2,1)).toEqual(1);
+    });
+
+    it('has a divide method', () => {
+        expect(myCalculator.divide).toBeDefined();
+    });
+
+    it('divides two numbers correctly', () => {
+        expect(myCalculator.divide(4,2)).toEqual(2);
+    });
+
+    it('throws an error when trying to divide by 0', () => {
+        expect(() => {
+            myCalculator.divide(1,0);
+        }).toThrow('division by zero');
+    });
+
+    it('has a multiply method', () => {
+        expect(myCalculator.multiply).toBeDefined();
+    });
+
+    it('multiplies two numbers correctly', () => {
+        expect(myCalculator.multiply(2,3)).toEqual(6);
+    });
+}) 
+
+describe('ceasarCipher', () => {
+
+    let myEncoder;
+
+    beforeEach(() => {
+        myEncoder = ceasarCipher();
+    });
+
+    it('is defined', () => {
+        expect(ceasarCipher).toBeDefined();
+    });
+
+    it('has an encode method', () => {
+        expect(myEncoder.encode).toBeDefined();
+    });
+
+    it('takes in a string and a shift factor', () => {
+        expect(() => {
+            myEncoder.encode(1, 3);
+        }).toThrow('first argument must be a string');
+                expect(() => {
+            myEncoder.encode('abc', 'abc');
+        }).toThrow('second argument must be a number');
+    });
+
+    it('returns a string', () => {
+        expect(typeof(myEncoder.encode('abc', 3))).toEqual('string');
+    });
+
+    it('correctly shifts the string', () => {
+        expect(myEncoder.encode('abc', 3)).toEqual('def');
+    });
+
+    it('wraps back to abc', () => {
+        expect(myEncoder.encode('xyz', 3)).toEqual('abc');
+    });
+
+    it('preserves the case', () => {
+        expect(myEncoder.encode('HeLLo', 3)).toEqual('KhOOr')
+    });
+
+    it('keeps the punctuation unchanged', () => {
+        expect(myEncoder.encode('Hello, World!', 3)).toEqual('Khoor, Zruog!')
+    });
+
 })
