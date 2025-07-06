0ee1a465dee85909c9555ae829cc44b1f286bb62
Valentin Semur <valentin.semur@gmail.com>
2025-07-06 11:44:31 +0200

    feat: setup TS template

---
+++ react/setting-up-a-react-environment/template_react-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  const items = ["Paris", "New-York", "London", "Tokyo", "Madrid"];
+
+	return (
+		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
+			<div className="bg-white p-4 rounded-lg shadow-md">
+				<ListGroup items={items} heading="Cities" />
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ react/setting-up-a-react-environment/template_react-ts/src/components/ListGroup.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+interface Props {
+    items: string[];
+    heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+	return (
+		<ul className="list">
+			<li className="p-4 pb-2 opacity-70 tracking-wide" key="title">
+				{heading}
+			</li>
+			{items.length === 0 && <p>No city found</p>}
+			{items.map((item, index) => (
+				<li key={index} className={selectedIndex === index ? "list-row bg-blue-500 text-white" : "list-row"} onClick={() => setSelectedIndex(index)}>
+					{item}
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default ListGroup;
